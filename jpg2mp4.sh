d8bdc47de1b487754c1faf169b9f2ad9ff31c94f
华桦 <hua.hua.hb5@is.naist.jp>
2021-05-22 14:46:14 +0800

    update

--- jpg2mp4.sh
+++ jpg2mp4.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-ffmpeg -r 20 -pattern_type glob -i 'output_result/a/*.jpg' -c:v libx264 -r 20 axu2cga-yolov3-vehicle_vol.1.mp4
+VIDEO_NAME1='bus-stop'
+VIDEO_NAME2='busy-street'
+VIDEO_NAME3='construction-workers'
+VIDEO_NAME4='night'
+VIDEO_NAME5='night2'
+VIDEO_NAME6='night3'
+VIDEO_NAME7='no-helmet-workers'
 
-#ffmpeg -r 20 -pattern_type glob -i 'output_result/b/*.jpg' -c:v libx264 -r 20 axu2cga-yolov3-vehicle_vol.2.mp4
-
-#ffmpeg -r 20 -pattern_type glob -i 'output_result/c/*.jpg' -c:v libx264 -r 20 axu2cga-yolov3-vehicle_vol.3.mp4
-
-#ffmpeg -r 20 -pattern_type glob -i 'output_result/d/*.jpg' -c:v libx264 -r 20 axu2cga-yolov3-vehicle_vol.4.mp4
-
-#ffmpeg -r 20 -pattern_type glob -i 'output_result/img/*.jpg' -c:v libx264 -r 20 axu2cga-yolov3-vehicle_vol.5.mp4
-
+ffmpeg -r 25 -pattern_type glob -i $VIDEO_NAME1'/*.jpg' -c:v libx264 -r 25 $VIDEO_NAME1.mp4
+ffmpeg -r 30 -pattern_type glob -i $VIDEO_NAME2'/*.jpg' -c:v libx264 -r 30 $VIDEO_NAME2.mp4
+ffmpeg -r 25 -pattern_type glob -i $VIDEO_NAME3'/*.jpg' -c:v libx264 -r 25 $VIDEO_NAME3.mp4
+ffmpeg -r 25 -pattern_type glob -i $VIDEO_NAME4'/*.jpg' -c:v libx264 -r 25 $VIDEO_NAME4.mp4
+ffmpeg -r 25 -pattern_type glob -i $VIDEO_NAME5'/*.jpg' -c:v libx264 -r 25 $VIDEO_NAME5.mp4
+ffmpeg -r 25 -pattern_type glob -i $VIDEO_NAME6'/*.jpg' -c:v libx264 -r 25 $VIDEO_NAME6.mp4
+ffmpeg -r 25 -pattern_type glob -i $VIDEO_NAME7'/*.jpg' -c:v libx264 -r 25 $VIDEO_NAME7.mp4
