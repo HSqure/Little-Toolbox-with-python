b046fa06382c76dc2762fbec9e38efcc1798b26d
华桦 <hua.hua.hb5@is.naist.jp>
2021-08-24 09:40:38 +0800

    Update file_scan.py

--- file_scan.py
+++ file_scan.py
@@ -5,17 +5,17 @@ import os
 """
 本函数用于查找目录下所有文件名
 """
-def file_name_list(file_dir):        
-    output=os.listdir(file_dir)
-    return output
+def file_name_scanner(file_dir):        
+    file_name_list=os.listdir(file_dir)
+    return file_name_list
 
 def _main():
 
     list_file = open('concrete_tests.txt', 'w') #生成的清单路径
 
-    FILE_DIR='JPEGImages' #标注文件路径
+    FILE_DIR = 'JPEGImages' #标注文件路径
 
-    file_list=file_name_list(FILE_DIR) #查找标注文件目录下的所有文件名并保存在file_list里
+    file_list = file_name_scanner(FILE_DIR) #查找标注文件目录下的所有文件名并保存在file_list里
 
     for x in file_list:
         list_file.write(x)
