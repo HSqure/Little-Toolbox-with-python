4e0669285a06f5f9b10e58fc46171e2ee566046b
华桦 <hua.hua.hb5@is.naist.jp>
2023-11-25 00:59:06 +0800

    Add files via upload

--- nodejs18_install.sh
+++ nodejs18_install.sh
@@ -8,3 +8,6 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg 
 NODE_MAJOR=18
 ARCH=amd64
 echo "deb [arch=$ARCH signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+sudo apt-get update
+sudo apt-get install nodejs -y
