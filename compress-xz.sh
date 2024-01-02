2c0e0e85066c7759883acf7abc25705f7b15e7e2
华桦 <hua.hua.hb5@is.naist.jp>
2024-01-02 17:20:51 +0800

    Update compress-xz.sh

--- compress-xz.sh
+++ compress-xz.sh
@@ -6,4 +6,4 @@
 TRD_NUM=0 # set 0 to allow maximoum cpu treads number
 COMPRESS_GRADE=9 # 1-9
 
-tar -cvf - ${1} | xz -9T 0 - > ${1}.tar.xz
+tar -cvf - ${1} | xz -${COMPRESS_GRADE}T ${TRD_NUM} - > ${1}.tar.xz
